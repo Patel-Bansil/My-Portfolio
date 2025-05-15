39a053e9e25309de1eae1fac73f91be186264ea6
Bansil Patel <bansilpatel162@gmail.com>
2025-05-16 01:55:30 +0530

    dark-light mode feature added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-/* <-============= Typing Feature ============->*/ 
+/* <-============= Typing Feature ============->*/
 
-let typed = new Typed(".typing",{
-    strings:["HTML Developer","CSS Developer","JS Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+let typed = new Typed(".typing", {
+  strings: ["HTML Developer", "CSS Developer", "JS Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
 
-/* <-============= Smooth Scrolling Feature ============->*/ 
+/* <-============= Smooth Scrolling Feature ============->*/
 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('.scroll-section'),
-    smooth: true
+  el: document.querySelector(".scroll-section"),
+  smooth: true,
 });
 
 /*
@@ -32,21 +32,20 @@ document.querySelectorAll('.nav a').forEach(function(link) {
 });
 */
 
-
-document.querySelectorAll(".nav a").forEach((link)=>{
-    link.addEventListener("click",function(e){
-        // console.log(value.target.getAttribute("href"));
-        let targetedID = e.target.getAttribute("href");
-        if(targetedID.startsWith('#')){
-            e.preventDefault();
-            let findedSection = document.querySelector(targetedID);
-            if (findedSection) {
-                scroll.scrollTo(findedSection)
-            }            
-        }
-    })
-})
-/* <-============= Mouse Follower Feature ============->*/ 
+document.querySelectorAll(".nav a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    // console.log(value.target.getAttribute("href"));
+    let targetedID = e.target.getAttribute("href");
+    if (targetedID.startsWith("#")) {
+      e.preventDefault();
+      let findedSection = document.querySelector(targetedID);
+      if (findedSection) {
+        scroll.scrollTo(findedSection);
+      }
+    }
+  });
+});
+/* <-============= Mouse Follower Feature ============->*/
 
 Shery.mouseFollower({
   //Parameters are optional.
@@ -55,10 +54,38 @@ Shery.mouseFollower({
   duration: 0.5,
 });
 
+/* <-============= submit form Feature ============->*/
 
-let contactForm = document.querySelector('.contact-form')
-contactForm.addEventListener("submit",(e)=>{
+let contactForm = document.querySelector(".contact-form");
+contactForm.addEventListener("submit", (e) => {
   setTimeout(() => {
-    contactForm.reset()
+    contactForm.reset();
   }, 1000);
-})
+});
+
+/* <-============= dark-light mode Feature ============->*/
+
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.querySelector(".theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode")
+  localStorage.setItem("darkmode","active")
+};
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode")
+  localStorage.setItem("darkmode", null)
+};
+
+if (darkmode === "active") {
+  enableDarkmode()
+}
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode")
+  if (darkmode !== "active") {
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
